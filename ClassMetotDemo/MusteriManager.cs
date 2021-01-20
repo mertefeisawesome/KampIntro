0bec286beb1853164bd86efd42b561aab963b0f2
Mert <mertefeisawesome@gmail.com>
2021-01-20 19:02:40 +0300

    Ödev 3

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle (ref Musteri musteri, ref int ID)
+        {
+            ID++; //ana programda tanımladığımız ID değişkenini metodun içinde kullanıp her yeni kullanıcıda bir artırıyoruz
+            musteri.ID = ID;
+            Console.WriteLine("Yeni Müşteri Ekleme");
+            Console.Write("Müşteri Adını Giriniz: ");
+            musteri.Ad = Console.ReadLine();
+            Console.Write("Müşteri Soyadını Giriniz: ");
+            musteri.Soyad = Console.ReadLine();
+            Console.Write("Müşteri Telefon Numarasını Giriniz: ");
+            musteri.Telefon = Console.ReadLine();
+            Console.WriteLine("Kullanıcı sisteme eklendi!"); //Bunda diziye ekleme kısmını çözemediğim için iyi niyetli bir çalışma olabildi sadece :D
+        }
+
+        public void Sil (Musteri musteri)
+        {
+            Console.WriteLine("Kullanıcı sistemden silindi!"); //Biraz spam mail listelerinden çıkarken yazan unsubscribedı hatırlatmadı değil ama daha iyisini yazamadım :)
+        }
+
+        public void bilgileriniGoster(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri ID: " + musteri.ID + " için bilgiler:\n");
+            Console.WriteLine("Ad: " + musteri.Ad);
+            Console.WriteLine("Soyad: " + musteri.Soyad);
+            Console.WriteLine("Telefon Numarası: " + musteri.Telefon + "\n");
+            
+        }
+
+            public void tumunuListele (Musteri[] musteriler)
+        {
+            Console.WriteLine("Kayıtlı Müşterilerimiz");
+            
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Ad: " + musteri.Ad);
+                Console.WriteLine("Soyad: " + musteri.Soyad);
+                Console.WriteLine("Telefon Numarası: " + musteri.Telefon + "\n");
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ID = 2; //iki müşteriyi kendim tanımladığım için ID'yi de 2 olarak belirliyorum
+     
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Ad = "John";
+            musteri1.Soyad = "Doe";
+            musteri1.Telefon = "3565498655";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 2;
+            musteri2.Ad = "Jane";
+            musteri2.Soyad = "Doe";
+            musteri2.Telefon = "2654962356"; //iki müşteri tanımladık, şimdilik yeterli
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.bilgileriniGoster(musteri1);
+
+            Musteri musteri3 = new Musteri(); //yeni bir müşteriManager tanımladık. Ekle metoduyla üçüncü müşteriyi kullanıcıya tanımlatacağız
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 }; //diziye eleman eklemeyi bilmediğim için böyle biraz çirkin bir yola başvurdum
+
+            System.Threading.Thread.Sleep(1000); //daha insani bir his vermesi için araya bekleme süreleri koydum
+            musteriManager.Ekle(ref musteri3, ref ID); //üçüncü müşteriyi kullanıcıya tanımlatıyoruz
+            System.Threading.Thread.Sleep(1000);
+            musteriManager.tumunuListele(musteriler); //kullanıcının tanımladığı dahil tüm kullanıcıları listeliyoruz
+
+            Console.WriteLine(musteri3.ID); //output: 3. Kodumuz çalışmış oley!
+        }
+    }
+}
