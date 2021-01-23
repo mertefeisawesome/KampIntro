4e677c00ebdcbe27ff166f0f3eb3e687d45a1086
Mert <mertefeisawesome@gmail.com>
2021-01-23 20:22:37 +0300

    4. Gün - Ödev 5

---
+++ MyDictionaryClass/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryClass
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private List<TKey> _keys;
+        private List<TValue> _values;
+
+        public MyDictionary()
+        {
+            _keys = new List<TKey>();
+            _values = new List<TValue>();
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index = _keys.IndexOf(key);
+                TValue value = _values[index];
+                return value;
+            }
+
+        }
+        public TKey this[TValue val]
+        {
+            get
+            {
+                int index = _values.IndexOf(val);
+                TKey key = _keys[index];
+                return key;
+            }
+
+        }
+
+        public void Add(TKey key, TValue val)
+        {
+            _keys.Add(key);
+            _values.Add(val);
+        }
+    }
+}
--- OOP1/Program.cs
+++ OOP1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP1
 {
@@ -17,6 +18,15 @@ namespace OOP1
 
             ProductManager productManager = new ProductManager();
             productManager.Add(product1);
+
+            List<string> isimler2 = new List<string> { "Engin", "Murat", "Kerem", "Halil" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("İlker");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
 
         }
     }
